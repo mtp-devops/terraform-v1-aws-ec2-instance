47346585224e00a686ecaa2ea34adaf8d4884c97
Igor Yakovlev <igory@checkpoint.com>
2021-10-03 16:02:22 +0300

    init commit

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "aws_region" {
+  description = "The AWS region to deploy to (e.g. us-east-1)"
+}
+
 variable "create" {
   description = "Whether to create an instance"
   type        = bool
